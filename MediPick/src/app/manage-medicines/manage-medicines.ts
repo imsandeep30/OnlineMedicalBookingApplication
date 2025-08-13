f4425884a93f87de5efaf2e7356e59786f57d383
Darshan Upadhyay <2417547@cognizant.com>
2025-08-13 17:02:52 +0530

    Updated Manage madicine

--- MediPick/src/app/manage-medicines/manage-medicines.ts
+++ MediPick/src/app/manage-medicines/manage-medicines.ts
@@ -12,12 +12,10 @@ import { HttpClient,HttpHeaders } from '@angular/common/http';
 })
 export class ManageMedicines implements OnInit {
   searchTerm = '';
-  selectedCategory = '';
   selectedStatus = '';
   Math = Math;
 
   medicines: any[] = [];
-  categories: string[] = [];
   statuses = ['In Stock', 'Out of Stock', 'Low Stock'];
   editingMedicine: any = null;
   editFormData: any = {};
@@ -59,16 +57,12 @@ newMedicine: any = {
             name: `${med.medicineName} (${med.brand})`,
             rawName: med.medicineName, // keeping original name for delete API
             description: med.description,
-            category: 'General',
             price: med.price,
-            discount: 0,
             stock: med.quantityAvailable,
             status: status,
-            prescription: 'Not Required'
           };
         });
 
-        this.categories = [...new Set(this.medicines.map(m => m.category))];
       });
   }
 
@@ -185,8 +179,7 @@ deleteMedicine(medicineName: string) {
 
   get filteredMedicines() {
     return this.medicines.filter(m => {
-      return (!this.searchTerm || m.name.toLowerCase().includes(this.searchTerm.toLowerCase()) || m.category.toLowerCase().includes(this.searchTerm.toLowerCase())) &&
-             (!this.selectedCategory || m.category === this.selectedCategory) &&
+      return (!this.searchTerm || m.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ) &&
              (!this.selectedStatus || m.status === this.selectedStatus);
     });
   }
